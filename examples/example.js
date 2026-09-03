2d4c9b512d205c574486f18148721feacdf5a109
Klemen Kastelic <klemen.kast@gmail.com>
2026-09-03 17:22:33 +0200

    - updated example

--- examples/example.js
+++ examples/example.js
@@ -1,58 +1,67 @@
 const updater = require('../nwjs-updater');
 const package = require('../package.json');
 
-const url = "";
-const headers = {}
+const url = "https://pos.dev.olaii.com/api/v1/registers/misc/package.json";
+const headers = {
+  "Authorization": "RegisterToken VxwwiqQwzyqLhpaZTMV0nBI5ZFiIkjvzoLL3Q7pwY9ysZxlYjwymTKAvHj1GnIyyo",
+  "Content-Type": "application/json",
+  "X-PosId": "krneki",
+}
 
-// ------------------ 1 ------------------
-updater.checkVersion(url, headers).then(function (newManifest) {
-  // console.log(package);
+const args = updater.getArgs();
+console.log("[UPDATER] Parsed arguments:", );
 
-  // ------------------ 2 ------------------
-  if (updater.isThereNewVersion(package.version, newManifest)) {
+
+async function runUpdater() {
+  // ------------------ 1 ------------------
+  try {
+    const newManifest = await updater.checkVersion(url, headers);
+
+    // ------------------ 2 ------------------
+    if (!updater.isThereNewVersion(package.version, newManifest.version)) {
+      throw console.error("No new update!");
+    }
 
     // ------------------ 3 ------------------
-    updater.download(newManifest, function (downloadStatus) {
+    const downloadResponse = await updater.download(newManifest, function (downloadStatus) {
       // Download status
-    }).then(function (downloadResponse) {
-
-      // ------------------ 4 ------------------
-      updater.unpack(downloadResponse, newManifest, function (unpackStatus) {
-        // Unpack status
-      }).then(function (unpackResponse) {
-
-        // ------------------ 5 ------------------
-        updater.runInstaller(newManifest);
-      }, function (error) {
-        console.error(error);
-      });
-
-    }, function (error) {
-      console.error(error);
     });
 
-  } else {
-    console.error("No new update!");
-  }
-}, function (error) {
-  console.error("Cannot fetch new manifest:", error);
-});
-
-
-
-function runInInstaller () {
-  if (gui.App.argv.length > 2) {
-    // ------------- 6 -------------
-    copyPath = gui.App.argv[0];
-    execPath = gui.App.argv[1];
-
-    // Replace old app, Run updated app from original location and close temp instance
-    updater.install(copyPath).then(function () {
-      // ------------- 7 -------------
-      const client = updater.run(execPath, null);
-      client.on('spawn', function () {
-        gui.App.quit();
-      })
+    // ------------------ 4 ------------------
+    const unpackResponse = await updater.unpack(downloadResponse, newManifest, function (unpackStatus) {
+      // Unpack status
     });
+
+    // ------------------ 5 ------------------
+    updater.runInstaller(newManifest);
+  } catch (error) {
+    console.error("Error:", error);
   }
 }
+
+
+async function runInInstaller () {
+  if (process.argv.length > 2) {
+    // ------------- 6 -------------
+    copyPath = args.appPath;
+    execPath = args.appExec;
+
+    // Replace old app, Run updated app from original location and close temp instance
+    await updater.install(copyPath, process.pid, function (installStatus) {
+      // Install status
+    });
+
+    // ------------- 7 -------------
+    const runArgs = updater.packArgs({
+      action: "UPDATED"
+    });
+    const client = updater.run(execPath, runArgs);
+    client.on('spawn', function () {
+      gui.App.quit();
+    });
+  } else {
+    runUpdater();
+  }
+}
+
+runInInstaller();
